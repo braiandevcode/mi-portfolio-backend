ff379f2dbf13a958e23a867058187da0f5d02600
Braian Palacios <braianpalacios699@gmail.com>
2025-05-09 06:52:27 -0300

    cambios en gitignore

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,17 +1,17 @@
 import { createPool } from "mysql2/promise";
-import dotenv from 'dotenv';
-dotenv.config(); //USAMOS CONFIGURACION DE DOTENV PARA LEER VARIABLES DE ENTORNO 
+import dotenv from "dotenv";
+dotenv.config(); //USAMOS CONFIGURACION DE DOTENV PARA LEER VARIABLES DE ENTORNO
 
 // CONEXION A LA BASE DE DATOS MYSQL
 const DB_CONNECTION = createPool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  database: process.env.DB_NAME,
-  password: process.env.PASSWORD,
-  port: process.env.DB_PORT,
-  waitForConnections: true,     // esperará si todas las conexiones están ocupadas
-  connectionLimit: 10,          // máximo 10 conexiones simultáneas
-  queueLimit: 0                 // sin límite de espera en cola
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  waitForConnections: true, // esperará si todas las conexiones están ocupadas
+  connectionLimit: 10, // máximo 10 conexiones simultáneas
+  queueLimit: 0, // sin límite de espera en cola
 });
 
 export default DB_CONNECTION;
